8b121d27867626afc8cdba6b0a2b58a155994c62
driesva <Dries.VanAutreve@telenet.be>
2019-04-05 07:24:16 +0200

    WEBC-26 / APOCOW-1 vl-ui-form-message - vervolledigen implementatie - tests - demo

---
+++ vl-form-message.src.js
@@ -0,0 +1,24 @@
+import { NativeVlElement } from '/node_modules/vl-ui-core/vl-core.js';
+
+/**
+ * vl-ui-form-message
+ *
+ * @demo demo/vl-ui-form-message.html
+ */
+export class VlFormMessage extends NativeVlElement(HTMLLabelElement) {
+
+  static get _observedClassAttributes() {
+    return ['light', 'block'];
+  }
+
+  connectedCallback() {
+    this.classList.add('vl-form__label');
+  }
+
+  get _classPrefix() {
+    return 'vl-form__label--';
+  }
+  
+}
+
+customElements.define('vl-form-message', VlFormMessage, {extends: 'label'});
