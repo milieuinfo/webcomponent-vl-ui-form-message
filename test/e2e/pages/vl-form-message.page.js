4df38c61acdab91d26affb62d2662b50c271add1
cambiph <incubusattax@gmail.com>
2019-12-03 15:25:43 +0100

    Initiele opzet

---
+++ test/e2e/pages/vl-form-message.page.js
@@ -0,0 +1,15 @@
+const VlFormMessage = require('../components/vl-form-message');
+const { Page } = require('vl-ui-core');
+const { Config } = require('vl-ui-core');
+
+class VlFormMessage extends Page {
+    async _getFormMessage(selector) {
+        return new VlFormMessage(this.driver, selector);
+    }
+
+    async load() {
+        await super.load(Config.baseUrl + '/demo/vl-form-message.html');
+    }
+}
+
+module.exports = VlFormMessage;
