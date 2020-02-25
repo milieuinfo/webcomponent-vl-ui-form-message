c9376cd571c61191ad0f370590a4399f62d531e4
wautergu <guy.wauters@vlaanderen.be>
2020-02-25 13:04:58 +0100

    index.js aanpassen omdat e2e componenten gewijzigd zijn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 module.exports = { 
     Test: {
-        VlFormMesssage: require('./test/e2e/components/vl-form-message')
+        VlFormAnnotation: require('./test/e2e/components/vl-form-annotation'),
+        VlFormLabel: require('./test/e2e/components/vl-form-label'),
+        VlFormValidationMessage: require('./test/e2e/components/vl-form-validation-message')
     }
 }
