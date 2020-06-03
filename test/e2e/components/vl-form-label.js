eb7690ab084f08cced20269653a445f40471fa75
Tom Coemans <tom.coemans@gmail.com>
2020-06-03 12:55:01 +0200

    ESlint

--- test/e2e/components/vl-form-label.js
+++ test/e2e/components/vl-form-label.js
@@ -1,19 +1,17 @@
-const { VlElement } = require('vl-ui-core').Test;
+const {VlElement} = require('vl-ui-core').Test;
 
-class VlFormLabel extends VlElement {  
+class VlFormLabel extends VlElement {
+  async isLight() {
+    return this.hasAttribute('light');
+  }
 
-    async isLight() {
-        return this.hasAttribute('light');
-    }
+  async isBlock() {
+    return this.hasAttribute('block');
+  }
 
-    async isBlock() {
-        return this.hasAttribute('block');
-    }
-    
-    async isFor() {
-    	return this.getAttribute("for");
-    }
-    
+  async isFor() {
+    return this.getAttribute('for');
+  }
 }
 
 module.exports = VlFormLabel;
--- test/e2e/pages/vl-form-message.page.js
+++ test/e2e/pages/vl-form-message.page.js
@@ -1,58 +1,56 @@
 const VlFormLabel = require('../components/vl-form-label');
 const VlFormAnnotation = require('../components/vl-form-annotation');
 const VlFormValidationMessage = require('../components/vl-form-validation-message');
-const { Page, Config } = require('vl-ui-core').Test;
+const {Page, Config} = require('vl-ui-core').Test;
 
 class VlFormMessagePage extends Page {
-   
+  async _getFormLabel(selector) {
+    return new VlFormLabel(this.driver, selector);
+  }
 
-    async _getFormLabel(selector) {
-        return new VlFormLabel(this.driver, selector);
-    }
+  async _getFormAnnotation(selector) {
+    return new VlFormAnnotation(this.driver, selector);
+  }
 
-    async _getFormAnnotation(selector) {
-        return new VlFormAnnotation(this.driver, selector);
-    }
+  async _getFormValidationMessage(selector) {
+    return new VlFormValidationMessage(this.driver, selector);
+  }
 
-    async _getFormValidationMessage(selector) {
-        return new VlFormValidationMessage(this.driver, selector);
-    }
-    
-    async getFormLabel() {
-        return this._getFormLabel('#form-label');
-    }
+  async getFormLabel() {
+    return this._getFormLabel('#form-label');
+  }
 
-    async getFormLabelLight() {
-        return this._getFormLabel('#form-label-light');
-    }
+  async getFormLabelLight() {
+    return this._getFormLabel('#form-label-light');
+  }
 
-    async getFormLabelBlock() {
-        return this._getFormLabel('#form-label-block');
-    }
+  async getFormLabelBlock() {
+    return this._getFormLabel('#form-label-block');
+  }
 
-    async getFormAnnotation() {
-        return this._getFormAnnotation('#form-annotation');
-    }
+  async getFormAnnotation() {
+    return this._getFormAnnotation('#form-annotation');
+  }
 
-    async getFormAnnotationBlock() {
-        return this._getFormAnnotation('#form-annotation-block');
-    }
+  async getFormAnnotationBlock() {
+    return this._getFormAnnotation('#form-annotation-block');
+  }
 
-    async getFormValidationMessageError() {
-        return this._getFormValidationMessage('#form-validation-message-error');
-    }
+  async getFormValidationMessageError() {
+    return this._getFormValidationMessage('#form-validation-message-error');
+  }
 
-    async getFormValidationMessageBlockError() {
-        return this._getFormValidationMessage('#form-validation-message-block-error');
-    }
+  async getFormValidationMessageBlockError() {
+    return this._getFormValidationMessage('#form-validation-message-block-error');
+  }
 
-    async getFormValidationMessageSuccess() {
-        return this._getFormValidationMessage('#form-validation-message-success');
-    }
-    
-    async load() {
-        await super.load(Config.baseUrl + '/demo/vl-form-message.html');
-    }
+  async getFormValidationMessageSuccess() {
+    return this._getFormValidationMessage('#form-validation-message-success');
+  }
+
+  async load() {
+    await super.load(Config.baseUrl + '/demo/vl-form-message.html');
+  }
 }
 
 module.exports = VlFormMessagePage;
