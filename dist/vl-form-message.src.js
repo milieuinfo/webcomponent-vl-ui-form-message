a30457f96e9057a540d00adeeec8f7d396d26183
coemans <tom.coemans@gmail.com>
2020-07-16 16:41:12 +0200

    update dependencies (#71)

--- dist/vl-form-message.src.js
+++ dist/vl-form-message.src.js
@@ -6,7 +6,6 @@ import {nativeVlElement, define} from 'vl-ui-core';
  * @classdesc Gebruik de vl-form-label om labels toe te voegen aan een formulier.
  *
  * @extends HTMLLabelElement
- * @mixin nativeVlElement
  *
  * @property {boolean} light - Attribuut wordt gebruikt om het label in een lichte kleur te tonen.
  * @property {boolean} block - Attribuut wordt gebruikt om het label in block vorm te tonen zodat het de breedte van het parent element aanneemt.
@@ -35,7 +34,6 @@ export class VlFormLabel extends nativeVlElement(HTMLLabelElement) {
  * @classdesc Gebruik de vl-form-validation-message om een validatie boodschap toe te voegen aan een formulier.
  *
  * @extends HTMLParagraphElement
- * @mixin nativeVlElement
  *
  * @property {boolean} error - Attribuut wordt gebruikt om foutboodschap te tonen bij validatiefouten.
  * @property {boolean} success - Attribuut wordt gebruikt om boodschap te tonen bij een succesvolle validatie.
@@ -98,7 +96,6 @@ export class VlFormValidationMessage extends nativeVlElement(HTMLParagraphElemen
  * @classdesc Gebruik de vl-form-annotation om invoerinstructies toe te voegen aan een formulier.
  *
  * @extends HTMLParagraphElement
- * @mixin nativeVlElement
  *
  * @property {boolean} block - Attribuut wordt gebruikt om het label in block vorm te tonen zodat het de breedte van het parent element aanneemt.
  *
