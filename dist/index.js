e8c1fc3dc649b46fcbf9da18dbd742d718fce207
Tom Coemans <tom.coemans@gmail.com>
2020-08-14 09:00:07 +0200

    dist cache

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  VlFormMessage: require('../dist/vl-form-message.src.js').VlFormMessage,
-  VlFormValidationMessage: require('../dist/vl-form-message.src.js').VlFormValidationMessage,
-  VlFormAnnotation: require('../dist/vl-form-message.src.js').VlFormAnnotation,
-};
